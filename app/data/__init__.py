bf632d8193ac054846cdd8ebd5e56ae5cc286735
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2026-02-22 19:55:22 +0600

    resolved

--- app/data/__init__.py
+++ app/data/__init__.py
@@ -1,8 +1,5 @@
-from .chatgpt_image import ChatgptImage
-from .chatgpt_llm import ChatgptLLM
 from .comfy_client import ComfyClient
 from .coqui_tts import CoquiTTS
-from .elevenlab_tts import ElevenlabTTS
 from .ollama_llm import OllamaLLM
 
 __all__ = [
